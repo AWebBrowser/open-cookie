6ec890f5d26e419b5011fe056876b2932de37182
AWebBrowser <gabrielito0604@gmail.com>
2017-10-30 16:06:54 -1000

    Initial Commit, added basics, unsure if works.

---
+++ cookie.js
@@ -0,0 +1,14 @@
+var totalCookies = 0;
+var showCookies = document.getElementById('numberOfCookies').innerHTML = totalCookies + " Cookies"
+
+if(totalCookies > 1000000000000){
+	window.alert("Hello Hacker :)");
+	totalCookies = 0;
+}
+
+while (totalCookies > 0){
+	totalCookies + cookiesPs;
+}
+function addCookie(){
+	totalCookies++;
+};
