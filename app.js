cb4ce1ed3f26e1bc8a7c8de026d63e5b457c503b
AWebBrowser <gabrielito0604@gmail.com>
2017-11-09 16:21:45 -1000

    Figured out how to add upgrades :/

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var value = parseInt(document.getElementById('numberOfCookies').value, 10);
-var changeText = document.setInterval(incrementPerSecond,1);
-function incrementValue(){
-    	value = isNaN(value) ? 0 : value;
-    	value++;
-    	document.getElementById('numberOfCookies').value = value;
-	}
-function incrementPerSecond() {
-	return incrementValue();
+var value = document.getElementById('numberOfCookies').value;
+var upg1 = 0;
+var upg2 = 0;
+var upg3 = 0;
+function incrementValue() {
+	value++;
+	return document.getElementById('numberOfCookies').value = value
+}
+function buyUpg1(){
+	var upgradeCost = Math.floor(10 * Math.pow(1.1, cursors))
 }
