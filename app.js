df8a26b800d87e3be81f4062625ef211bfbe38b7
Jan Armendariz-Bones <30916174+AWebBrowser@users.noreply.github.com>
2017-11-13 16:26:09 -1000

    Done with upgrades

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var value = document.getElementById('numberOfCookies').value;
-var upg1 = 0;
-var upg2 = 0;
-var upg3 = 0;
-function incrementValue() {
-	value++;
-	return document.getElementById('numberOfCookies').value = value
+var value = parseInt(document.getElementById('numberOfCookies').value);
+var cursors = 0;
+var grandmothers = 0;
+var ImpossibleThing = 0;
+var cookiesPerClick = 0;
+
+function incrementValue(number) {
+	value += number;
+	return document.getElementById('numberOfCookies').value = value;
 }
 function buyUpg1(){
-	var upgradeCost = Math.floor(10 * Math.pow(1.1, cursors))
+	document.getElementById('buyCursor').innerHTML = "Buy a cursor for " + upgradeCost + " cookies";
+	var upgradeCost = Math.floor(10 * Math.pow(1.1, cursors));
+	if (value >= upgradeCost) {
+		cursors += 1;
+		value -= upgradeCost;
+		document.getElementById('amountOfCursors').innerHTML = cursors;
+		document.getElementById('numberOfCookies').value = value;
+		upgradeCost = Math.floor(10 * Math.pow(1.1, cursors));
+	}else if(upgradeCost >= value){
+		alert("You do not have enough cookies to buy this upgrade");
+	}
+	console.log("upgradeCost:", upgradeCost)
+	document.getElementById('buyCursor').innerHTML = "Buy a cursor for " + upgradeCost + " cookies"
+	console.log(cursors);
+	console.log(value);
 }
