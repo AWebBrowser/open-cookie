19b60254b19223255dd184634396e4248146ad1c
AWebBrowser <gabrielito0604@gmail.com>
2017-11-20 15:48:41 -1000

    semi-fixed rogue decimals

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var cursors = 0;
 var grandmothers = 0;
 var ImpossibleThing = 0;
 var cPC = 1;
+var cPS = 0;
 var savegame = {
     cookies: cookies,
     cursors: cursors,
     grandmothers: grandmothers,
     ImpossibleThing: ImpossibleThing
 };
-
 var savegame = JSON.parse(localStorage.getItem("save"));
 
 function incrementValue(number) {
@@ -26,7 +26,7 @@ function buyUpg1(){
 		document.getElementById('aOC').innerHTML = cursors;
 		document.getElementById('nOC').value = cookies;
 		upgradeCost = Math.floor(10 * Math.pow(1.1, cursors));
-
+		cPS += 0.5;
 	}else if(upgradeCost >= cookies){
 		alert("You do not have enough cookies to buy this upgrade");
 	}
@@ -34,10 +34,10 @@ function buyUpg1(){
 	document.getElementById('bC').innerHTML = "Buy a cursor for " + upgradeCost + " cookies";
 	console.log(cursors);
 	console.log(cookies);
+	console.log(cPS);
 }
-
-function fix(input){
-    var output = Math.round(input * 123456789)/123456789;
-    return output;
-}
-document.getElementById('nOC').innerHTML = fix(cookies);
+/* cPS  Method */
+setInterval(function(){
+	cookies += cPS;
+	document.getElementById('nOC').value = cookies;
+},1000);
