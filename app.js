c847d7ee220af66e454f8ac036e0769afa24eca0
AWebBrowser <gabrielito0604@gmail.com>
2017-11-20 16:21:50 -1000

    4:20PM 20/11/2017

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var savegame = {
     ImpossibleThing: ImpossibleThing
 };
 var savegame = JSON.parse(localStorage.getItem("save"));
-
+function saveButton(){}
+function deleteSave(){}
 function incrementValue(number) {
 	cookies += number;
 	return document.getElementById('nOC').value = cookies;
@@ -35,13 +36,13 @@ function buyUpg1(){
 }
 function buyUpg2(){
 	document.getElementById('bC').innerHTML = "Buy a cursor for " + upgradeCost + " cookies";
-	var upgradeCost = Math.floor(10 * Math.pow(1.1, cursors));
+	var upgradeCost = Math.floor(50 * Math.pow(1.1, grandmothers));
 	if (cookies >= upgradeCost) {
-		cursors += 1;
+		grandmothers += 1;
 		cookies -= upgradeCost;
-		document.getElementById('aOC').innerHTML = cursors;
-		document.getElementById('nOC').value = cookies;
-		upgradeCost = Math.floor(10 * Math.pow(1.1, cursors));
+		document.getElementById('aOG').innerHTML = cursors;
+		document.getElementById('nOG').value = cookies;
+		upgradeCost = Math.floor(50 * Math.pow(1.1, grandmothers));
 		cPS += 0.5;
 	}else if(upgradeCost >= cookies){
 		alert("You do not have enough cookies to buy this upgrade");
@@ -53,3 +54,9 @@ setInterval(function(){
 	cookies += cPS;
 	document.getElementById('nOC').value = cookies;
 },1000);
+/* Easter Egg Maker */
+function easterEgg(number){
+	alert("You have found easter egg number " + number + "!");
+	alert("A 50% boost has been added to your clicks!");
+	cPC = cPC * 1.5;
+};
